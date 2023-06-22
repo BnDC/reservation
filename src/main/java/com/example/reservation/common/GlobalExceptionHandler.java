e17e45b0db8388e6a01970dbfdc1b4066fcac01c
bufflect <bndgc@naver.com>
2023-06-22 21:03:34 +0900

    [Mod] ExceptionHandler logging 수정

--- src/main/java/com/example/reservation/common/GlobalExceptionHandler.java
+++ src/main/java/com/example/reservation/common/GlobalExceptionHandler.java
@@ -1,5 +1,7 @@
 package com.example.reservation.common;
 
+import javax.persistence.EntityNotFoundException;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.BadCredentialsException;
@@ -12,28 +14,31 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 @RestControllerAdvice
 public class GlobalExceptionHandler {
-
 	@ExceptionHandler({BadCredentialsException.class, UsernameNotFoundException.class})
 	public ResponseEntity<String> unAuthorizedExceptionHandler(Exception e) {
-		log.info("BadCredentialsException handled message= {}", e.getMessage());
+		printException(e);
 		return new ResponseEntity<>(e.getMessage(), HttpStatus.UNAUTHORIZED);
 	}
 
-	@ExceptionHandler(IllegalArgumentException.class)
-	public ResponseEntity<String> illegalArgumentExceptionHandler(IllegalArgumentException e) {
-		log.info("IllegalArgumentException handled message= {}", e.getMessage());
+	@ExceptionHandler({IllegalArgumentException.class, EntityNotFoundException.class})
+	public ResponseEntity<String> badRequestExceptionHandler(Exception e) {
+		printException(e);
 		return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
 	}
 
 	@ExceptionHandler(RuntimeException.class)
 	public ResponseEntity<String> runtimeExceptionHandler(RuntimeException e) {
-		log.info("RuntimeException handled message= {}", e.getMessage());
+		printException(e);
 		return new ResponseEntity<>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
 	}
 
 	@ExceptionHandler(Exception.class)
 	public ResponseEntity<String> exceptionHandler(Exception e) {
-		log.info("Exception handled message= {}", e.getMessage());
+		printException(e);
 		return new ResponseEntity<>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+	}
+
+	private void printException(Exception e) {
+		log.info("{} handled", e.toString());
 	}
 }
