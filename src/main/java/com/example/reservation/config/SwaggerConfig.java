2de565ea7c70cb1cd0a184db4c9a9f963063ce73
bufflect <bndgc@naver.com>
2023-06-27 15:55:14 +0900

    [Add] springfox swagger 의존성 추가

---
+++ src/main/java/com/example/reservation/config/SwaggerConfig.java
@@ -0,0 +1,36 @@
+package com.example.reservation.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.RestController;
+
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+
+@Configuration
+public class SwaggerConfig {
+	private static final String version = "1.0";
+
+	@Bean
+	public Docket api() {
+		return new Docket(DocumentationType.SWAGGER_2)
+				.select()
+				.apis(RequestHandlerSelectors.withClassAnnotation(RestController.class))
+				.paths(PathSelectors.any())
+				.build()
+				.apiInfo(apiInfo());
+	}
+
+	private ApiInfo apiInfo() {
+		return new ApiInfoBuilder()
+				.title("영화 예매")
+				.description("영화 예매를 위한 시스템 구축")
+				.version(version)
+				.build();
+	}
+
+}
