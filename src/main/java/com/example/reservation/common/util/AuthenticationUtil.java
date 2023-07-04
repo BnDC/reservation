22011f6f3fdd83b099e6c8de7ba133bf61c6091c
bufflect <bndgc@naver.com>
2023-07-04 14:12:24 +0900

    [Mod] userDetails 인증 객체가 없을 때, Exception 수정  - IllegalArgumentException -> InsufficientAuthenticationException

--- src/main/java/com/example/reservation/common/util/AuthenticationUtil.java
+++ src/main/java/com/example/reservation/common/util/AuthenticationUtil.java
@@ -2,12 +2,16 @@ package com.example.reservation.common.util;
 
 import java.util.Collection;
 
+import org.springframework.security.authentication.InsufficientAuthenticationException;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
 
 import com.example.reservation.domain.member.model.CustomUserDetails;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 public class AuthenticationUtil {
 	public static String getUsername() {
 		return getUserDetails().getUsername();
@@ -24,7 +28,8 @@ public class AuthenticationUtil {
 	private static CustomUserDetails getUserDetails() {
 		Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
 		if (authentication == null || authentication.getPrincipal().equals("anonymousUser")) {
-			throw new IllegalArgumentException("인증되지 않은 객체입니다");
+			log.debug("[AuthenticationUtil] can't get userDetails");
+			throw new InsufficientAuthenticationException("It should have authenticated");
 		}
 		return (CustomUserDetails)authentication.getPrincipal();
 	}
